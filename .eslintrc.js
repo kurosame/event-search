c0bddeb980cb07a90ab2cc7a96e397e5a785b72b
kurosame <kurosame1227@gmail.com>
2019-05-30 11:47:43 +0900

    fix: eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,24 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@typescript-eslint/parser'
   },
   extends: [
+    'airbnb-base',
     '@nuxtjs',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
     'prettier',
     'prettier/vue'
   ],
-  plugins: [
-    'prettier'
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/core-modules': ['vue', 'vuex']
+  },
   // add your custom rules here
   rules: {
-    'nuxt/no-cjs-in-config': 'off'
+    'nuxt/no-cjs-in-config': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error'
   }
 }
