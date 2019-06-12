cbd75d00e38b92c3e2145a5f765dc76e2df16201
kurosame <kurosame1227@gmail.com>
2019-06-12 22:39:07 +0900

    add: search in table

--- components/Summary.vue
+++ components/Summary.vue
@@ -1,22 +1,36 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    :pagination.sync="pagination"
-    :loading="loading"
-  >
-    <template #items="{ item }">
-      <td>{{ item.title }}</td>
-      <td>{{ item.catch }}</td>
-      <td>{{ item.description }}</td>
-      <td>
-        <a :href="item.eventUrl" target="_blank">{{ item.eventUrl }}</a>
-      </td>
-      <td>{{ item.startedAt }}</td>
-      <td>{{ item.endedAt }}</td>
-      <td>{{ item.address }}</td>
-    </template>
-  </v-data-table>
+  <v-card>
+    <v-card-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        placeholder="nuxt 東京 etc.."
+        append-icon="search"
+        hide-details
+      />
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :search="search"
+      :rows-per-page-items="[10, 50, 100]"
+      :pagination.sync="pagination"
+      :loading="loading"
+    >
+      <template #items="{ item }">
+        <td>{{ item.title }}</td>
+        <td>{{ item.catch }}</td>
+        <td>{{ item.description }}</td>
+        <td>
+          <a :href="item.eventUrl" target="_blank">{{ item.eventUrl }}</a>
+        </td>
+        <td>{{ item.startedAt }}</td>
+        <td>{{ item.endedAt }}</td>
+        <td>{{ item.address }}</td>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 <script lang="ts">
 import StoreHelper from '@/components/mixins/StoreHelper.vue'
@@ -27,6 +41,8 @@ import chunk from 'lodash/chunk'
 
 @Component
 class Summary extends Mixins(StoreHelper) {
+  search: string = ''
+
   pagination: { sortBy: string } = { sortBy: 'startedAt' }
 
   loading: boolean = true
